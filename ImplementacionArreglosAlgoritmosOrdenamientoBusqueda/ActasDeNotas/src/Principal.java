7ce8b0a84ec838b460de6630e59819f697b9a9f2
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-22 22:20:29 -0500

    Principal.java

--- ImplementacionArreglosAlgoritmosOrdenamientoBusqueda/ActasDeNotas/src/Principal.java
+++ ImplementacionArreglosAlgoritmosOrdenamientoBusqueda/ActasDeNotas/src/Principal.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import HojaMatricula.Estudiante;
+import java.util.Scanner;
+
+public class Principal {
+    
+   public static void main(String[] args) {
+        Principal p = new Principal();
+        ClaseProcesos proc = new ClaseProcesos();
+       proc.pedirDatosActa();
+       proc.leerDatosActa();
+        
+        ActaNotas acta = new ActaNotas("Programacion I","2024","Ingenieria en Computacion","Diurno",30849,"1M11-Co","C1006","1006",28);
+        proc.registrarNotas();
+       Estudiante est = new Estudiante();
+       
+        System.out.println(ClaseProcesos.estudiantes.get(0).getNoCarnet());
+        System.out.println(ClaseProcesos.estudiantes.get(0).getNombres());
+        System.out.println(ClaseProcesos.estudiantes.get(0).getListaNotas().get(0).getPp());
+       
+       proc.escribirActa(acta);
+        proc.leerDatosActa();
+        
+        
+        
     }
 }
